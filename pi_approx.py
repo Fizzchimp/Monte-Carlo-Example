dba433a4c8c4710f68c80c4eaab241fad87bcb74
Callum Askew <18askewc@staidans.co.uk>
2024-05-15 13:31:14 +0100

    dpated

--- pi_approx.py
+++ pi_approx.py
@@ -2,34 +2,38 @@
 import random
 from math import sqrt
 
-line = {"x": [], "y": []}
+def main():
+    line = [[], []]
 
-iters = 100000
-for i in range(iters + 1):
-    x = i / iters
-    line["x"].append(x)
-    line["y"].append(sqrt(1 - x ** 2))
+    iters = 100000000
+    for i in range(iters + 1):
+        num = i / iters
+        line[0].append(num)
+        line[1].append(sqrt(1 - num ** 2))
     
 
-bPoints = [[], []]
-aPoints = [[], []]
-for i in range(iters):
-    x, y = random.randint(0, iters), random.randint(0, iters) / iters
+    bPoints = [[], []]
+    aPoints = [[], []]
+    for i in range(iters):
+        x, y = random.randint(0, iters), random.randint(0, iters) / iters
     
-    if line["y"][x] > y:
-        bPoints[0].append(x / iters)
-        bPoints[1].append(y)
+        if line[1][x] > y:
+            bPoints[0].append(x / iters)
+            bPoints[1].append(y)
         
-    if line["y"][x] < y:
-        aPoints[0].append(x / iters)
-        aPoints[1].append(y)
+        if line[1][x] < y:
+            aPoints[0].append(x / iters)
+            aPoints[1].append(y)
         
         
-plt.plot(line["x"], line["y"], color = "r", label = "f(x) = √(1 - x²)")
-plt.scatter(bPoints[0], bPoints[1], color = "y", s = iters / 10000000, label = "Below f(x)")
-plt.scatter(aPoints[0], aPoints[1], color = "b", s = iters / 10000000, label = "Above f(x)")
+    plt.plot(line[0], line[1], color = "r", label = "f(x) = √(1 - x²)")
+    plt.scatter(bPoints[0], bPoints[1], color = "y", s = 400 / iters, label = "b: Below f(x)")
+    plt.scatter(aPoints[0], aPoints[1], color = "b", s = 400 / iters, label = "a: Above f(x)")
 
-pi = 4 * len(bPoints[0]) / iters
-plt.title(f"Pi ≈ {pi}")
-plt.legend(shadow = True, markerscale = 50)
-plt.show()
+
+    pi = 4 * len(bPoints[0]) / iters
+    plt.title(f"Pi ≈ {pi}")
+    plt.legend(shadow = True, markerscale = 50, loc = "upper right")
+    plt.show()
+    
+main()
